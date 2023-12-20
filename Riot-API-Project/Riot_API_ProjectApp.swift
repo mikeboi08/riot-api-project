e1524a2e7df30a3e4b95a6a7dd39f64972c214de
Micah Howard <micahhoward99@gmail.com>
2023-12-20 16:20:51 -0500

    Initial Commit

---
+++ Riot-API-Project/Riot_API_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Riot_API_ProjectApp.swift
+//  Riot-API-Project
+//
+//  Created by Micah Howard on 12/20/23.
+//
+
+import SwiftUI
+
+@main
+struct Riot_API_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
